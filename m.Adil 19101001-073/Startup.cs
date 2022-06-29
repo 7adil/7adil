f9083d1dca3118be9fb76b2b5e900fcf79cc63e1
7adil <103042895+7adil@users.noreply.github.com>
2022-06-29 09:10:25 +0500

    missing file upload
    missing line upload

---
+++ m.Adil 19101001-073/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace m.Adil_19101001_073
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+
+
+           
+          
+           
+           
+        }
+    }
+}
